d3084c3d638c1a81bdd7130d8091275861413dfe
Jennifer Chinzi <jenrose87@gmail.com>
2020-07-13 15:55:27 -0700

    Add app.listen to server.js

--- server.js
+++ server.js
@@ -8,5 +8,24 @@ require('dotenv').config();
 
 const cors = require('cors');
 
+app.use(cors());
+
 const PORT = process.env.PORT || 3001;
 
+// Routes
+
+// {
+//   "search_query": "seattle",
+//   "formatted_query": "Seattle, WA, USA",
+//   "latitude": "47.606210",
+//   "longitude": "-122.332071"
+// }
+
+// app.get('/location', (request, response) => {
+//   console.log(request.query);
+//   let city = request.query.city;
+// })
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+})
